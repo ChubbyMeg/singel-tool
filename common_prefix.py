b7e2fb99ea3cbf71f727eef16b8a18885254dd66
ChubbyMeg <106139761+ChubbyMeg@users.noreply.github.com>
2022-05-24 14:53:50 +0800

    Update common_prefix.py

--- common_prefix.py
+++ common_prefix.py
@@ -4,26 +4,26 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+        #1.目前prefix比現在的字串長
+        #2.目前prefix比現在的字串短
+        #3.目前prefix跟現在的字串一模一樣
         common_prefix = strs[0]
+        if common_prefix == "":
+            return ""
         for i in range(1,len(strs)):
-            if common_prefix == "":
-                break
-            j = 0
-            isBreak = 0
-            for j in range(len(common_prefix)):
-                if (j > len(strs[i])-1):
-                    isBreak = 1
-                    break
-                if common_prefix[j] != strs[i][j]:
-                    isBreak = 1
-                    break
-            print "i={},j={}".format(i,j)
-            print "strs[i][:j+1]={}".format(strs[i][:j+1])
-            print "strs[i][:j]={}".format(strs[i][:j])
-            if isBreak == 1:
-                common_prefix = strs[i][:j]
+            if len(common_prefix) <= len(strs[i]):
+                max_len = len(common_prefix)
             else:
-                common_prefix = strs[i][:j+1]
+                max_len = len(strs[i])
+            j = 0
+            for j in range(0,max_len):
+                if common_prefix[j] != strs[i][j]:
+                    j -= 1
+                    break
+            print("curren prefix={}".format(common_prefix))
+            print("j={}".format(j))
+            common_prefix = strs[i][:j+1]
+            print("new prefix={}".format(common_prefix))
             if common_prefix == "":
                 break
         return common_prefix
